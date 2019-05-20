8a28776c8c94d9ad0cd5fc514aa80e1d5a3173d6
hkshawn <34892023+hkshawn@users.noreply.github.com>
2019-05-20 14:08:07 +0800

    Add files via upload

---
+++ ipdeny.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while true
+do
+iplist=`netstat -ntu | awk '{print $5}'| cut -d':' -f1| sort |uniq -c |grep -v 'LISTEN|127.0.0.1'| sed 'N;$d;P;D' | awk '{if($1>50)print $2}'`
+for ip in $iplist
+do 
+  echo $ip
+  iptables -I INPUT -s $ip -j DROP
+  echo "$ip is drop!" && echo "$ip is drop!" >> /tmp/droplog
+  date && sleep 2
+
+done
+done
